39a8fd2e81dc5495d16d176841ff08d22eec83d1
gjp <1435842616@qq.com>
2020-07-05 17:18:24 +0800

    添加 AMap ElementUI Echarts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,16 @@
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    <div id="container" style="width:500px; height:300px"></div>
+    <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
@@ -31,10 +41,49 @@
 </template>
 
 <script>
+import AMapLoader from '@amap/amap-jsapi-loader';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      map:null,
+    }
+  },
+  mounted(){
+    AMapLoader.load({
+        "key": "c456a66f90bbed06c6214bc9d5168fcb",   // 申请好的Web端开发者Key，首次调用 load 时必填
+        "version": "2.0",   // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+        "plugins": []  //插件列表
+    }).then((AMap)=>{
+        this.map = new AMap.Map('container');
+    }).catch(e => {
+        console.log(e);
+    })
+    this.drawLine();
+  },
+  methods: {
+    drawLine(){
+        // 基于准备好的dom，初始化echarts实例
+        let myChart = this.$echarts.init(document.getElementById('myChart'))
+        // 绘制图表
+        myChart.setOption({
+            title: { text: '在Vue中使用echarts' },
+            tooltip: {},
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        });
+    }
   }
 }
 </script>
